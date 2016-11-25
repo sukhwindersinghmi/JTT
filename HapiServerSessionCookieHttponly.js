805997a04140d56fa941e5d57fa82ae93aadacb3
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-25 17:23:54 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -4,7 +4,7 @@ var Hapi = require('hapi');var cryptiles = require('cryptiles');var Blankie = re
 
 var server = new Hapi.Server();server.app.key = 'secret_app_value_102';server.connection({  port: port});
 
-server.register([  {    register: require('hapi-server-session'),    options: {      key: cryptiles.randomString(16),      expiresIn: 60000,      cookie: {        isHttpOnly: false,        isSecure: false      }    }  },{    register: Inert,    options: {}  },{    register: Scooter,    options: {}  },{    register: Blankie,    options: {scriptSrc: 'self'}  }], function (err) {  if (err) {    throw err;  }
+server.register([  {    register: require('hapi-server-session'),    options: {      key: cryptiles.randomString(16),      expiresIn: 60000,      cookie: {        isSecure: false      }    }  },{    register: Inert,    options: {}  },{    register: Scooter,    options: {}  },{    register: Blankie,    options: {scriptSrc: 'self'}  }], function (err) {  if (err) {    throw err;  }
 
 server.route({    method: 'GET',    path: '/',    handler: function (request, reply) {      reply('Hello World');    }  });});
 
