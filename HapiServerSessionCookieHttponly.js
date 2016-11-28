3ecd79cc399bbe3fa4112c46c79c9ca0d9769c33
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-28 13:02:03 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -6,8 +6,10 @@ const Joi = require('joi');
 var Blankie = require('blankie');
 var Scooter = require('scooter');
 const Inert = require('inert');
-
 const server = new Hapi.Server();
+
+var workFactor = 20;
+
 server.app.key = 'secret_app_value_102';
 server.connection({
 port: 3000
@@ -31,7 +33,7 @@ throw err;
 
 server.route({
 method: 'POST',
-path: '/negative/pbkdf2/literal/{password*}',
+path: '/negative/pbkdf2/variable/{password*}',
 config: {
 validate: {
 params: {
@@ -40,7 +42,7 @@ password: Joi.string().max(128).min(8).alphanum()
 },
 handler: function (request, reply) {
 const salt = myCrypto.randomBytes(256).toString('hex');
-myCrypto.pbkdf2(request.params.password, salt, 10, 512, 'sha512', function (err, hash) {
+myCrypto.pbkdf2(request.params.password, salt, workFactor, 512, 'sha512', function (err, hash) {
 if (err) throw err;
 reply(hash.toString('base64'));
 });
@@ -50,7 +52,7 @@ reply(hash.toString('base64'));
 
 server.route({
 method: 'POST',
-path: '/negative/pbkdf2Sync/literal/{password*}',
+path: '/negative/pbkdf2Sync/variable/{password*}',
 config: {
 validate: {
 params: {
@@ -59,7 +61,7 @@ password: Joi.string().max(128).min(8).alphanum()
 },
 handler: function (request, reply) {
 const salt = myCrypto.randomBytes(256).toString('hex');
-myCrypto.pbkdf2Sync(request.params.password, salt, 10, 512, 'sha512', function (err, hash) {
+myCrypto.pbkdf2Sync(request.params.password, salt, workFactor, 512, 'sha512', function (err, hash) {
 if (err) throw err;
 reply(hash.toString('base64'));
 });
