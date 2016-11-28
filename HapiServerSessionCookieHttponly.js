1d4b773dc27ad05d8d88b3cf580da30f6ece6e8a
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-28 13:27:50 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,6 +1,18 @@
 'use strict';
 const hapi = require('hapi');
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+
 const server = new hapi.Server();
+
+const users = {
+john: {
+username: 'john',
+password: '$2a$10$iqJSHD.BGr0E2IxQwYgJmeP3NvhPrXAeLSaGCj6IR/XU5QtjVu5Tm', // 'secret'
+name: 'John Doe',
+id: '2133d32a'
+}
+};
 
 const port = 3000;
 server.connection({
@@ -11,28 +23,36 @@ port: port,
 
 server.register([
 {
-register: require('crumb'),
-options: {
-cookieOptions: {
-isSecure: true
-}
-}
+register: require('hapi-auth-basic')
 }
 ], function (err) {
 if (err) {
 throw err;
 }
 
+server.auth.strategy('simple', 'basic', {
+validateFunc: function (request, username, password, callback) {
+const user = users[username];
+if (!user) {
+return callback(null, false);
+}
+bcrypt.compare(password, user.password, function (err, isValid) {
+callback(err, isValid, {
+id: user.id,
+name: user.name
+});
+});
+}
+});
+
 server.route({
 method: 'GET',
-path: '/cors/origin/3',
+path: '/auth',
 config: {
-cors: {
-origin: ['http://www.facebook.com']
-}
-},
+auth: 'simple',
 handler: function (request, reply) {
-reply('Hello World');
+reply('hello, ' + request.auth.credentials.name);
+}
 }
 });
 });
