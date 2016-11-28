ab2fb7834279fea94e59e7ae3985aa654877caae
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-28 17:52:38 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,25 +1,28 @@
+'use strict';
+
 var express = require('express');
-var session = require('express-session')
+var expSess = require("express-session");
 var app = express();
 
-app.disable('x-powered-by');
-app.use(session({
-secret: 'some secrete code',
-resave: false,
-saveUninitialized: true
-}));
+var sess = {
+secret: 'keyboard cat',
+key: "sessionId",
+resave: true,
+saveUninitialized: true,
+cookie: {
+httpOnly: false,
+secure: true
+}
+};
 
-app.get('/awesome', function(req, res) {
-req.session.lastPage = '/awesome';
-res.send('Your Awesome.');
+app.disable('X-Powered-By');
+app.use(expSess(sess));
+
+app.get('/', function (req, res) {
+res.send('Hello World');
 });
 
-app.get('/next', function(req, res){
-console.log("go to the next page "+ req.session.lastPage);//app.locals.url);
-var _url = req.session.lastPage;
-//redirect user to the value from req.session, but prepend it with protocol and host
-res.redirect(302, 'https://' + req.hostname + '/' + req.session.lastPage);
+var server = app.listen(3000, function () {
+var port = server.address().port;
+console.log('Your app listening at http://localhost:%s', port);
 });
-
-app.listen(3000);
-console.log("Server running on port 3000");
