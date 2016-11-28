728347292efa08043c347caf5b4877dd038af762
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-28 13:12:40 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -14,7 +14,7 @@ port: port,
 
 server.route({
 method: 'POST',
-path: '/positive/pbkdf2/positive1/{password*}',
+path: '/negative/pbkdf2/usalt/{password*}',
 config: {
 validate: {
 params: {
@@ -22,30 +22,9 @@ password: Joi.string().max(128).min(8).alphanum()
 }
 },
 handler: function (request, reply) {
-const salt = Crypto.randomBytes(256).toString('hex');
-Crypto.pbkdf2(request.params.password, salt, 100000, 512, 'sha512', (err, hash) => {
+Crypto.pbkdf2(request.params.password, request.params.salt, 100000, 512, 'sha512', function (err, hash) {
 if (err) throw err;
 reply(hash.toString('base64'));
-});
-}
-}
-});
-
-server.route({
-method: 'POST',
-path: '/positive/pbkdf2/positive2/{password*}',
-config: {
-validate: {
-params: {
-password: Joi.string().max(128).min(8).alphanum()
-}
-},
-handler: function (request, reply) {
-Crypto.randomBytes(256, (err, salt) => {
-Crypto.pbkdf2(request.params.password, salt, 100000, 512, 'sha512', (err, hash) => {
-if (err) throw err;
-reply(hash.toString('base64'));
-});
 });
 }
 }
