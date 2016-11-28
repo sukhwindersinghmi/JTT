41b287174d9d614ed999b68e92b937b1cf98cb86
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-28 12:34:45 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,55 +1,70 @@
-const hapi = require('hapi');
-const server = new hapi.Server();
-var cryptiles = require('cryptiles');
-var Bcrypt = require('bcrypt');
-const Basic = require('hapi-auth-basic');
-const Inert = require('inert');
-var crypto = require('crypto');
-
 'use strict';
 
+const Hapi = require('hapi');
+const myCrypto = require('crypto');
+const Joi = require('joi');
+var Blankie = require('blankie');
+var Scooter = require('scooter');
+const Inert = require('inert');
+
+const server = new Hapi.Server();
+server.app.key = 'secret_app_value_102';
 server.connection({
-host: 'localhost',
-address: '127.0.0.1',
-port: 3000,
+port: 3000
 });
 
-//*******************************************************************************************************
-//******************** Start of trigger for Hapi Adaptive One Way Hash Password Storage *****************
-//*******************************************************************************************************
-// Create a in memory collections of users
-var users = {
-jane: {
-username: 'jane',
-password: '$2a$10$XPk.7lupEzBSHxUg/IavSuIKmwmpBbW0NfCL8q0ZfHXUPXTtbhmNK', // 'password'
-name: 'Jane Doe',
-id: '2133d32a'
+server.register([{
+register: Inert,
+options: {}
+},{
+register: Scooter,
+options: {}
+},{
+register: Blankie,
+options: {scriptSrc: 'self'}
+}],
+function (err) {
+if (err) {
+throw err;
 }
-};
-// Create a validation function for strategy
-var validate = function (username, password, callback) {
-var user = users[username];
-if (!user) {
-return callback(null, false);
-}
-
-};
-
-// Add the basic-auth plug-in
-server.register(require('hapi-auth-basic'), function (err) {
-server.auth.strategy('simple', 'basic', { validateFunc: validate });
 });
 
 server.route({
-method: 'GET',
-path: '/test/{password*}',
-config: { auth: 'simple' },
+method: 'POST',
+path: '/negative/pbkdf2/literal/{password*}',
+config: {
+validate: {
+params: {
+password: Joi.string().max(128).min(8).alphanum()
+}
+},
 handler: function (request, reply) {
-reply(request.params.hash);
+const salt = myCrypto.randomBytes(256).toString('hex');
+myCrypto.pbkdf2(request.params.password, salt, 10, 512, 'sha512', function (err, hash) {
+if (err) throw err;
+reply(hash.toString('base64'));
+});
+}
 }
 });
-//********************************************************************************************
-//***************End of trigger for Hapi Adaptive One Way Hash Password Storage***************
-//********************************************************************************************
 
-server.start();
+server.route({
+method: 'POST',
+path: '/negative/pbkdf2Sync/literal/{password*}',
+config: {
+validate: {
+params: {
+password: Joi.string().max(128).min(8).alphanum()
+}
+},
+handler: function (request, reply) {
+const salt = myCrypto.randomBytes(256).toString('hex');
+myCrypto.pbkdf2Sync(request.params.password, salt, 10, 512, 'sha512', function (err, hash) {
+if (err) throw err;
+reply(hash.toString('base64'));
+});
+}
+}
+});
+
+server.start(function () {});
