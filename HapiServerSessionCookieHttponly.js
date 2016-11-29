fb517115aaf3dac0f9c4b1ad87d70eb32dc950f4
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-29 12:52:23 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -3,25 +3,28 @@
 var express = require('express');
 var expSess = require("express-session");
 var app = express();
+var bodyParser = require('body-parser');
 
-var sess = {
-secret: 'keyboard cat',
-key: "sessionId",
-resave: true,
-saveUninitialized: true,
-cookie: {
-httpOnly: true
-}
-};
+// support json encoded bodies
+app.use(bodyParser.json());
+// support encoded bodies
+app.use(bodyParser.urlencoded({extended: true}));
 
+// Register templating engine
+app.engine('html', require('ejs').renderFile);
+app.set("view engine", "html");
+app.set("views", __dirname + "/static/views");
+app.use(express.static(__dirname + '/static'));
 app.disable('x-powered-by');
-app.use(expSess(sess));
 
-app.get('/', function (req, res) {
-res.send('Hello World');
-});
+// ======= for MongoDB mass assignment test =======
+//require('./massassignment/test.insert-one-param')(app);
+//require('./massassignment/test.insert-two-params')(app);
+require('./massassignment/test.positive-mongoose')(app);
 
-var server = app.listen(3000, function () {
-var port = server.address().port;
-console.log('Your app listening at http://localhost:%s', port);
+
+
+var server = app.listen(3000, function() {
+  var port = server.address().port;
+  console.log('Your app listening at http://localhost:%s', port);
 });
