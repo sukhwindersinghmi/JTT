f9eb8718f2ea1ce7b8ca0e5b587f6bdbf4b5b3cb
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-29 14:23:33 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,47 +1,22 @@
-<html>
-<head>
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="index.js"></script>
-<script>
-// Trigger 1 - Math.Random
-function makeRandom1() {
-var min = 1;
-var max = 10;
-// 1st HIT
-return Math.floor(Math.random() * (max - min) + min);
+const Hapi = require('hapi');
+const server = new Hapi.Server();
+server.connection({
+host: 'localhost',
+port: 8000
+});
+server.route({
+method: 'GET',
+path: '/hello',
+config: {
+handler: function (request, reply) {
+reply({foo: 'bar'});
+},
+jsonp: 'callback'
 }
-
-    // Trigger 2 - RandomSource.getRandomValues test
-    function makeRandom2() {
-        var array = new Uint32Array(1);
-        // 2nd HIT
-        var rnd2 = window.crypto.getRandomValues(array);
-        // 3rd HIT
-        var rnd3 = crypto.getRandomValues([]);
-        console.log("window.crypto.getRandomValues(string)");
-        return rnd2[0] + rnd3[1];
-    }
-</script>
-<meta name="description" content="Free Web tutorials">
-<meta name="keywords" content="HTML,CSS,XML,JavaScript">
-<script src="//www.google-analytics.com/analytics.js"></script>
-<meta name="author" content="Hege Refsnes">
-<script>
-    $(document).ready(function() {
-        $('#usrfld').val('userid' + makeRandom1());
-        $.ajax({
-            type: "POST",
-            url: '/submit',
-            data: {
-                "usrfld": "userid" + makeRandom2()
-            },
-            success: null,
-            dataType: 'json'
-        });
-    });
-</script>
-
-</head>
-<body>
-</body>
-</html>
+});
+server.start((err) => {
+if (err) {
+throw err;
+}
+console.log('Server running at:', server.info.uri);
+});
