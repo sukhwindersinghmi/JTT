c76cf14936d4e89509a367de546eeaaf59089556
sukhwindersinghmi <sukhwinder.singh@3pillarglobal.com>
2016-11-29 12:57:23 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,22 +1,53 @@
+document.ready(function () {
+console.log("Hello World!");
+});
 
+The .html code is as below:
 
-'use strict';
-
-var express = require('express');
-var app = express();
-
-app.disable('x-powered-by');
-
+<html>
+<head>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="index.js"></script>
+<script>
+// Trigger 1 - Math.Random
+function makeRandom1() {
 var min = 1;
 var max = 10;
-var rnd1 = Math.floor(Math.random() * (max - min) + min - Math.random());
+// 1st HIT
+return Math.floor(Math.random() * (max - min) + min);
+}
 
-app.post('/', function(req, res) {
-req.session.sessionId = max < rnd1 ? rnd1: max;
-res.send('PRNG test');
-});
+    // Trigger 2 - RandomSource.getRandomValues test
+    function makeRandom2() {
+        var array = new Uint32Array(1);
+        // 2nd HIT
+        var rnd2 = window.crypto.getRandomValues(array);
+        // 3rd HIT
+        var rnd3 = crypto.getRandomValues([]);
+        console.log("window.crypto.getRandomValues(string)");
+        return rnd2[0] + rnd3[1];
+    }
+</script>
+<meta name="description" content="Free Web tutorials">
+<meta name="keywords" content="HTML,CSS,XML,JavaScript">
+<script src="//www.google-analytics.com/analytics.js"></script>
+<meta name="author" content="Hege Refsnes">
+<script>
+    $(document).ready(function() {
+        $('#usrfld').val('userid' + makeRandom1());
+        $.ajax({
+            type: "POST",
+            url: '/submit',
+            data: {
+                "usrfld": "userid" + makeRandom2()
+            },
+            success: null,
+            dataType: 'json'
+        });
+    });
+</script>
 
-var server = app.listen(3000, function () {
-var port = server.address().port;
-console.log('Your app listening at http://localhost:%s', port);
-});
+</head>
+<body>
+</body>
+</html>
