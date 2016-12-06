e2739db37a4b035f8554654492388219404ae44c
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-06 14:28:18 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -3,8 +3,8 @@ var app = express();
 app.disable('x-powered-by');
 
 app.get('/command', function(req, res){
-if (typeof req.params.input == "number") {
-app.locals.myvalue = req.params.input;
-}
-
+if(req.params.input=="true")
+app.locals.myvalue = "1";
+else if (req.params.input=="false")
+app.locals.myvalue = "0";
 });
