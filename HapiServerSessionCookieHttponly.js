107b3a36a539f14c60cdb5952bf85454f6faeaba
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-06 14:41:56 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,37 +1,25 @@
 var express = require('express');
+var session = require('express-session')
 var app = express();
 
 app.disable('x-powered-by');
-app.use(express.static(__dirname + '/public'));
+app.use(session({
+secret: 'some secrete code',
+resave: false,
+saveUninitialized: true
+}));
 
-app.post('/url',function(req, res){
-//store value from user input in app.locals
-app.locals.url = "http://tst.com";
-console.log(("/url: "+url));
-res.send('GET /url response');
+app.get('/awesome', function(req, res) {
+req.session.lastPage = '/awesome';
+res.send('Your Awesome.');
 });
 
 app.get('/next', function(req, res){
-console.log("go to the next page "+app.locals.url);
-var url = app.locals.url;
-//redirect user to the value from app.locals, but prepend it with protocol and host
-console.log(("URL: "+url));
-res.redirect(302, 'https://' + req.hostname + '/' + url);
+console.log("go to the next page "+ req.session.lastPage);//app.locals.url);
+var _url = req.session.lastPage;
+//redirect user to the value from req.session, but prepend it with protocol and host
+res.redirect(302, 'https://' + req.hostname + '/' + req.session.lastPage);
 });
-
-app.post('/next2', function(req, res){
-console.log("go to the next page "+app.locals.url);
-var _url = app.locals.url;
-//Diferent ways to concatenate
-var _redirectURL = req.protocol;
-_redirectURL += '' + req.hostname + '/';
-res.redirect(302, _redirectURL);
-});
-
-app.post('/next3', function(req, res){
-var url = app.locals.url; //Using concat function var _redirectURL = 'https://'.concat(req.hostname,'/', url); console.log("go to the next page "+redirectURL);
-res.redirect(302, _redirectURL);
-})
 
 app.listen(3000);
 console.log("Server running on port 3000");
