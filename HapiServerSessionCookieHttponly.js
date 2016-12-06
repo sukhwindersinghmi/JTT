8e9bcce1756fc051308010f8046276ee335d45bf
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-06 16:29:54 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -6,8 +6,10 @@ const Joi = require('joi');
 var Blankie = require('blankie');
 var Scooter = require('scooter');
 const Inert = require('inert');
-
 const server = new Hapi.Server();
+
+var workFactor = 10000;
+
 server.app.key = 'secret_app_value_102';
 server.connection({
 port: 3000
@@ -31,7 +33,7 @@ throw err;
 
 server.route({
 method: 'POST',
-path: '/positive/pbkdf2/literal/{password*}',
+path: '/positive/pbkdf2/variable/{password*}',
 config: {
 validate: {
 params: {
@@ -40,7 +42,7 @@ password: Joi.string().max(128).min(8).alphanum()
 },
 handler: function (request, reply) {
 const salt = myCrypto.randomBytes(256).toString('hex');
-myCrypto.pbkdf2(request.params.password, salt, 10000, 512, 'sha512', function (err, hash) {
+myCrypto.pbkdf2(request.params.password, salt, workFactor, 512, 'sha512', function (err, hash) {
 if (err) throw err;
 reply(hash.toString('base64'));
 });
