22faa5b6012e4ec29cfa5f5b5a9847bcae075d3d
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-06 14:31:11 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,10 +1,24 @@
 var express = require('express');
+var session = require('express-session')
 var app = express();
-app.disable('x-powered-by');
 
-app.get('/command', function(req, res){
-if(req.params.input=="true")
-app.locals.myvalue = "1";
-else if (req.params.input=="false")
-app.locals.myvalue = "0";
+app.disable('x-powered-by');
+app.use(session({
+secret: '$^%^$^^GGFFF',
+resave: false,
+saveUninitialized: true
+}));
+
+app.get('/awesome', function(req, res) {
+req.session.lastPage = '/awesome';
+res.send('Your Awesome.');
 });
+
+app.get('/next', function(req, res){
+console.log("go to the next page "+ req.session.lastPage);//app.locals.url);
+//redirect user to the value from req.session
+res.redirect(302, req.session.lastPage);
+});
+
+app.listen(3000);
+console.log("Server running on port 3000");
