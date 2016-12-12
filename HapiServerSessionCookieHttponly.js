fc7de3af0d5a047d6c1118e352a95d34d653131b
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 12:58:37 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,16 +1,14 @@
 'use strict';
 
 const Hapi = require('hapi');
-const myCrypto = require('crypto');
+const scrypt = require('scrypt');
 const Joi = require('joi');
 var Blankie = require('blankie');
 var Scooter = require('scooter');
 const Inert = require('inert');
+
 const server = new Hapi.Server();
-
-var workFactor = 20;
-
-server.app.key = 'secret_app_value_102';
+server.app.key = 'super_secret';
 server.connection({
 port: 3000
 });
@@ -33,7 +31,7 @@ throw err;
 
 server.route({
 method: 'POST',
-path: '/negative/pbkdf2/variable/{password*}',
+path: '/negative/kdf/literal/{password*}',
 config: {
 validate: {
 params: {
@@ -41,10 +39,11 @@ password: Joi.string().max(128).min(8).alphanum()
 }
 },
 handler: function (request, reply) {
-const salt = myCrypto.randomBytes(256).toString('hex');
-myCrypto.pbkdf2(request.params.password, salt, workFactor, 512, 'sha512', function (err, hash) {
+scrypt.params(0.05, (err, params) => {
+scrypt.kdf(request.params.password, scryptParameters, function(err, hash) {
 if (err) throw err;
 reply(hash.toString('base64'));
+});
 });
 }
 }
@@ -52,7 +51,7 @@ reply(hash.toString('base64'));
 
 server.route({
 method: 'POST',
-path: '/negative/pbkdf2Sync/variable/{password*}',
+path: '/negative/kdfSync/literal/{password*}',
 config: {
 validate: {
 params: {
@@ -60,13 +59,16 @@ password: Joi.string().max(128).min(8).alphanum()
 }
 },
 handler: function (request, reply) {
-const salt = myCrypto.randomBytes(256).toString('hex');
-myCrypto.pbkdf2Sync(request.params.password, salt, workFactor, 512, 'sha512', function (err, hash) {
+scrypt.params(0.05, (err, params) => {
+scrypt.kdfSync(request.params.password, scryptParameters, function(err, hash) {
 if (err) throw err;
 reply(hash.toString('base64'));
+});
 });
 }
 }
 });
 
-server.start(function () {});
+server.start(function () {
+console.log('Now Visit: http://localhost:' + port);
+});
