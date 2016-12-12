cdc281e4833c57b32f0279de5017fb2056405dfc
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 12:32:36 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,35 +1,55 @@
-const Path = require('path');
-const Hapi = require('hapi');
+'use strict';
+
+var Hapi = require('hapi');
+var cryptiles = require('cryptiles');
+var Blankie = require('blankie');
+var Scooter = require('scooter');
 const Inert = require('inert');
 
-const server = new Hapi.Server({
-connections: {
-routes: {
-files: {
-relativeTo: Path.join(__dirname, 'public')
-}
-}
-}
-});
-server.connection({ port: 3000 });
+var port = 3000; // process.env.PORT || 3000; // allow port to be set by environment
 
-server.register(Inert, () => {});
+var server = new Hapi.Server();
+server.app.key = 'secret_app_value_102';
+server.connection({
+port: port
+});
+
+server.register([
+{
+register: require('hapi-server-session'),
+options: {
+key: cryptiles.randomString(16),
+expiresIn: 60000,
+cookie: {
+isHttpOnly: false
+}
+}
+},{
+register: Inert,
+options: {}
+},{
+register: Scooter,
+options: {}
+},{
+register: Blankie,
+options: {scriptSrc: 'self'}
+}
+], function (err) {
+if (err) {
+throw err;
+}
 
 server.route({
 method: 'GET',
-path: '/documents/{user}/{file}',
-handler: function(request, reply) {
-var path = Path.join(request.params.user, request.params.file);
-return reply.file(path);
+path: '/',
+handler: function (request, reply) {
+reply('Hello World');
 }
 });
-
-server.start((err) => {
-
-if (err) {
-    throw err;
-}
-
-console.log('Server running at:', server.info.uri);
-
 });
+
+server.start(function () {
+console.log('Now Visit: http://localhost:' + port + '/');
+});
+
+module.exports = server;
