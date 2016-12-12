77c3ce2a4d22d19fdb8f5515d92a33a6cc30a896
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 12:52:51 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,29 +1,70 @@
-const hapi = require('hapi');
-const server = new hapi.Server();
-var cryptiles = require('cryptiles');
-var Bcrypt = require('bcrypt');
-const Basic = require('hapi-auth-basic');
-const Inert = require('inert');
-
 'use strict';
 
+const Hapi = require('hapi');
+const myCrypto = require('crypto');
+const Joi = require('joi');
+var Blankie = require('blankie');
+var Scooter = require('scooter');
+const Inert = require('inert');
+
+const server = new Hapi.Server();
+server.app.key = 'secret_app_value_102';
 server.connection({
-host: 'localhost',
-address: '127.0.0.1',
-port: 3000,
+port: 3000
+});
+
+server.register([{
+register: Inert,
+options: {}
+},{
+register: Scooter,
+options: {}
+},{
+register: Blankie,
+options: {scriptSrc: 'self'}
+}],
+function (err) {
+if (err) {
+throw err;
+}
 });
 
 server.route({
-method: 'GET',
-path: '/test/{password*}',
-//config: { auth: 'simple' },
-handler: function (request, reply) {
-//var name = request.auth.credentials.name
-//reply('hello ' + name);
-reply(Bcrypt.hashSync(request.params.password, request.params.hash));
-
+method: 'POST',
+path: '/negative/pbkdf2/literal/{password*}',
+config: {
+validate: {
+params: {
+password: Joi.string().max(128).min(8).alphanum()
 }
-
+},
+handler: function (request, reply) {
+const salt = myCrypto.randomBytes(256).toString('hex');
+myCrypto.pbkdf2(request.params.password, salt, 10, 512, 'sha512', function (err, hash) {
+if (err) throw err;
+reply(hash.toString('base64'));
+});
+}
+}
 });
 
-server.start();
+server.route({
+method: 'POST',
+path: '/negative/pbkdf2Sync/literal/{password*}',
+config: {
+validate: {
+params: {
+password: Joi.string().max(128).min(8).alphanum()
+}
+},
+handler: function (request, reply) {
+const salt = myCrypto.randomBytes(256).toString('hex');
+myCrypto.pbkdf2Sync(request.params.password, salt, 10, 512, 'sha512', function (err, hash) {
+if (err) throw err;
+reply(hash.toString('base64'));
+});
+}
+}
+});
+
+server.start(function () {});
