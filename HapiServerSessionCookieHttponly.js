a8bb53d1a2029b0d2b9fe19fe796d542ed41057c
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 14:34:32 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,29 +1,28 @@
-
+'use strict';
 
 var express = require('express');
+var expSess = require("express-session");
 var app = express();
-var bodyParser = require('body-parser');
 
-var validator = require('validator');
-var sanitize = require('mongo-sanitize');
-var helmet = require('helmet');
-
-app.disable('x-powered-by');
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.json());
-
-urlMap = {
-"home": "http://mysite.com/",
-"confirm": "http://mysite.com/confirmation/",
-"logout": "http://mysite.com/logout",
-"partner1": "http://partner1.com/"
+var sess = {
+secret: 'keyboard cat',
+key: "sessionId",
+resave: true,
+saveUninitialized: true,
+cookie: {
+httpOnly: false,
+secure: true
 }
+};
 
-app.post('/next', function(req, res){
-var url = urlMap[res.param.url];
-if (url != undefined) {
-res.redirect(urlMap[res.params.url]);
-} else {
-res.redirect(urlMap["home"]);
-}
+app.disable('X-Powered-By');
+app.use(expSess(sess));
+
+app.get('/', function (req, res) {
+res.send('Hello World');
+});
+
+var server = app.listen(3000, function () {
+var port = server.address().port;
+console.log('Your app listening at http://localhost:%s', port);
 });
