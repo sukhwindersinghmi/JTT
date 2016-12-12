18ec4f93221a4e80678497586c502219f98eb75d
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 13:56:34 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,5 +1,8 @@
 'use strict';
 const hapi = require('hapi');
+const Crypto = require('crypto');
+const Joi = require('joi');
+
 const server = new hapi.Server();
 
 const port = 3000;
@@ -9,27 +12,23 @@ address: '127.0.0.1',
 port: port,
 });
 
-server.register([
-{
-register: require('crumb'),
-options: {
-cookieOptions: {
-isSecure: true
-}
-}
-}
-], function (err) {
-if (err) {
-throw err;
-}
-
 server.route({
-method: 'GET',
-path: '/cors/origin/1',
-handler: function (request, reply) {
-reply('Hello World');
+method: 'POST',
+path: '/negative/pbkdf2/csalt/{password*}',
+config: {
+validate: {
+params: {
+password: Joi.string().max(128).min(8).alphanum()
 }
+},
+handler: function (request, reply) {
+const salt = 'static_salt';
+Crypto.pbkdf2(request.params.password, salt, 100000, 512, 'sha512', function (err, hash) {
+if (err) throw err;
+reply(hash.toString('base64'));
 });
+}
+}
 });
 
 server.start(function () {
