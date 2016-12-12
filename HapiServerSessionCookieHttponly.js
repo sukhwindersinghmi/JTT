3997c60b468f90836ad56ab43533ee464cf6ead3
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 14:10:22 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,18 +1,1 @@
-server.route({
-method: 'GET',
-path: '/hello',
-config: {
-handler: function (request, reply) {
-reply({foo: 'bar'});
-},
-jsonp: 'callback'
-}
-});
 
-// Start the server
-server.start((err) => {
-if (err) {
-throw err;
-}
-console.log('Server running at:', server.info.uri);
-});
