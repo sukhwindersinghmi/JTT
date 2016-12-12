9ca392aca06c7db86fb5a04f32d60b9bb78fc05e
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 13:02:11 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,72 +1,31 @@
+const hapi = require('hapi');
+const server = new hapi.Server();
+var cryptiles = require('cryptiles');
+var Bcrypt = require('bcrypt');
+const Basic = require('hapi-auth-basic');
+const Inert = require('inert');
+var crypto = require('crypto');
+
 'use strict';
 
-const Hapi = require('hapi');
-const scrypt = require('scrypt');
-const Joi = require('joi');
-var Blankie = require('blankie');
-var Scooter = require('scooter');
-const Inert = require('inert');
-
-const scryptParameters = scrypt.paramsSync(0.01);
-
-const server = new Hapi.Server();
-server.app.key = 'super_secret';
 server.connection({
-port: 3000
+host: 'localhost',
+address: '127.0.0.1',
+port: 3000,
 });
 
-server.register([{
-register: Inert,
-options: {}
-},{
-register: Scooter,
-options: {}
-},{
-register: Blankie,
-options: {scriptSrc: 'self'}
-}],
-function (err) {
-if (err) {
-throw err;
-}
-});
+//server.register(Inert, () => {});
+server.register(Inert, function () {});
 
 server.route({
-method: 'POST',
-path: '/negative/kdf/literal/{password*}',
-config: {
-validate: {
-params: {
-password: Joi.string().max(128).min(8).alphanum()
-}
-},
-handler: function (request, reply) {
-
-  scrypt.kdf(request.params.password, scryptParameters, function(err, hash) {
-    if (err) throw err;
-    reply(hash.toString('base64'));
-  });
-}
-
-}
-});
-
-server.route({
-method: 'POST',
-path: '/negative/kdfSync/literal/{password*}',
-config: {
-validate: {
-params: {
-password: Joi.string().max(128).min(8).alphanum()
-}
-},
-handler: function (request, reply) {
-var hash = scrypt.kdfSync(request.params.password, scryptParameters);
-reply(hash.toString('base64'));
+method: 'GET',
+path: '/{path*}',
+handler: {
+directory: {
+path: '../test.html',
+listing: true
 }
 }
 });
 
-server.start(function () {
-console.log('Now Visit: http://localhost:' + port);
-});
+server.start();
