c9fef9390b2a8137bd963d40d6eaa1da8a1c9b56
Sukhwinder Singh <sukhwinder.singh@3pillarglobal.com>
2016-12-12 14:20:31 +0530

    Update HapiServerSessionCookieHttponly.js

--- HapiServerSessionCookieHttponly.js
+++ HapiServerSessionCookieHttponly.js
@@ -1,41 +1,36 @@
-var express = require('express');
-var app = express();
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/exampleDb";
 
 app.disable('X-Powered-By');
 
-// Register templating engine
-app.engine('html', require('ejs').renderFile);
-app.set("view engine", "html");
-app.set("views", __dirname + "/insecure_script");
-
-app.get('/', function(req, res) {
-res.sendFile('index');
+app.get('/me', function(req, res) {
+const yr = req.params.yr;
+// add or remove comma separated "key": values given your JSON collection
+const jsonQuery = {
+"year": yr, _id: 0, "quarter": 1, "daily": 1, "sms": 1, "paid": 1
+};
+// -1 descending or 1 ascending
+const jsort = {
+"quarter": -1
+};
+MongoClient.connect(url, function(err, db) {
+if(err) {
+return console.dir(err);
+}
+db.collection("calendar", function(err, collection) {
+collection.find(jsonQuery).sort(jsort).toArray(function(err, items) {
+res.send(items);
+});
+});
+});
 });
 
-var server = app.listen(3000, function() {
-var port = server.address().port;
+const server = app.listen(3000, function () {
+const port = server.address().port;
 console.log('Your app listening at http://localhost:%s', port);
 });
-
-The insecure_script folder contains index.html file with the below code
-
-<html>
-<head>
-</head>
-<body>
-This is some text
-<p>
-This script doesn't fire the trigger
-<script src="js/stupid_client_script.js"></script>
-This script fires the trigger
-<script src="http://www.example.com/example.js"></script>
-<script src="http://www.wordpress.com/dod.js"></script>
-<script src="http://www.shoes.com/act.js"></script>
-This is the text src="http://www.example.com" in the HTML text, but not as part of script.
-</p>
-<p>
-<script src="https://www.example.com/securejs.js"></script>
-The last example is accessed over HTTPS and should be a "pat on the back."
-</p>
-</body>
-</html>
